f7e9b89062ce243868a484dd4afdafe6759b86e4
veena14cs <veena@bitjini.com>
2015-12-18 17:37:43 +0530

    udpated screen designs and add layout in progres

--- vzcards/app/src/main/java/com/bitjini/vzcards/AddActivity.java
+++ vzcards/app/src/main/java/com/bitjini/vzcards/AddActivity.java
@@ -1,11 +1,15 @@
 package com.bitjini.vzcards;
 
 import android.os.Bundle;
+import android.support.v4.app.FragmentManager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import android.support.v4.app.Fragment;
+import android.widget.Button;
+import android.widget.FrameLayout;
+
 /**
  * Created by VEENA on 12/7/2015.
  */
@@ -14,7 +18,28 @@ public class AddActivity extends android.support.v4.app.Fragment {
     public View onCreateView(LayoutInflater inflater,ViewGroup container,
                              Bundle savedInstanceState)
     {
-        View addView=inflater.inflate(R.layout.add_layout,container,false);
-        return addView;
+        View iHave = inflater.inflate(R.layout.add_layout, container, false);
+        Button iNeed=(Button) iHave.findViewById(R.id.ineed);
+
+
+        iNeed.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v)
+            {
+
+                Fragment needFragment = new iNeed_Activity();
+                // get the id of fragment
+                FrameLayout contentView = (FrameLayout) getActivity().findViewById(R.id.ihave_frame);
+
+                // Insert the fragment by replacing any existing fragment
+                FragmentManager fragmentManager = getFragmentManager();
+                fragmentManager.beginTransaction()
+                        .add(contentView.getId(), needFragment).addToBackStack(contentView.toString())
+                        .commit();
+
+            }
+        });
+
+        return iHave;
     }
 }
